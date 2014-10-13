bda6851bc1a035246d721c7a4d676bbc70a3b1a4
letrungthientrinh@gmail.com <letrungthientrinh@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-13 05:03:56 +0000

    /** 	 * Login systemAccount 	 * 	 * @param username 	 * @param password 	 * @return true if username.getPassword == password 	 */

--- FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
+++ FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Component;
 
 import com.csc.fresher.controller.EntityManagerFactoryUtil;
 import com.csc.fresher.domain.SystemAccount;
-
+/*@author TrinhLe
+ * 
+ */
 @Component
 public class SystemAccountDAO {
+
 	public List<SystemAccount> getSystemAccounts() {
 		EntityManager entityManager = EntityManagerFactoryUtil
 				.createEntityManager();
@@ -22,17 +25,57 @@ public class SystemAccountDAO {
 		List<SystemAccount> systemaccounts = null;
 
 		entityTransaction.begin();
-		/*try {*/
-			TypedQuery<SystemAccount> query = entityManager.createQuery(
-					"SELECT a FROM SystemAccount a", SystemAccount.class);
-			systemaccounts = query.getResultList();
-			
-			entityTransaction.commit();
-		/*} catch (Exception e) {
-			entityManager.close();
-		}*/
-		
-		
+		/* try { */
+		TypedQuery<SystemAccount> query = entityManager.createQuery(
+				"SELECT a FROM SystemAccount a", SystemAccount.class);
+		systemaccounts = query.getResultList();
+
+		entityTransaction.commit();
+		/*
+		 * } catch (Exception e) { entityManager.close(); }
+		 */
+
 		return systemaccounts;
+	}
+
+	/**
+	 * Login systemAccount 
+	 * 
+	 * @param username
+	 * @param password
+	 * @return true if username.getPassword == password
+	 */
+	public boolean getSystemAccount(String username, String password) {
+
+		// Obtains entity manager object
+		EntityManager entityManager = EntityManagerFactoryUtil
+				.createEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		// -----------Begin transaction-----------
+		boolean check = false;
+		try {
+			entityTransaction.begin();
+
+			TypedQuery<SystemAccount> query = entityManager
+					.createQuery(
+							"SELECT c FROM "
+									+ SystemAccount.class.getName()
+									+ " c Where c.username= :username and c.password= :password",
+									SystemAccount.class);
+			query.setParameter("username", username);
+			query.setParameter("password", password);
+			SystemAccount sys = query.getSingleResult();
+			System.out.println(sys.getUsername());
+			check = query.getResultList().size() > 0;
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityManager.close();
+		}
+		// -----------End transaction-----------
+
+		return check;
+
 	}
 }
