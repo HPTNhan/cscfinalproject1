86bd4418b9a9fa1988042c3aa401129403163465
letrungthientrinh@gmail.com <letrungthientrinh@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-13 05:03:37 +0000

    /** 	 * login systemAccount 	 * 	 * @param request 	 * @param model 	 * @return 	 */

---
+++ FinalProject1/src/main/java/com/csc/fresher/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.csc.fresher.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.csc.fresher.dao.SystemAccountDAO;
+
+/**@author TrinhLe
+ * 
+ *
+ */
+@Controller
+public class LoginController {
+	/**
+	 * login systemAccount
+	 * 
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/checkLogin")
+	public String getCustomer(HttpServletRequest request, Model model) {
+		// Read account info from request and save into Account object
+		HttpSession session = request.getSession();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String message = "";
+
+		// Create an AccountDAO
+		SystemAccountDAO systemDAO = new SystemAccountDAO();
+
+		// Query customer to DB
+		if(systemDAO.getSystemAccount(username, password)){
+			session.setAttribute("username", username);
+			message = "Success!";
+			model.addAttribute("message", message );
+			
+			return "home";
+		}
+		else{
+			
+			message = "Wrong ID or Password!";
+			model.addAttribute("message", message );
+			return "home";
+		}
+	}
+
+}
