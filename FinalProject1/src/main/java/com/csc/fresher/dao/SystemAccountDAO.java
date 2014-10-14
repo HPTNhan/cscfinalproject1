a4d79c58fa87d3363a9c9ebb9541e2d118726e18
letrungthientrinh <letrungthientrinh@gmail.com>
2014-10-14 09:30:51 +0000

    /**@author TrinhLe 	 * search account for 7 fields condition 	 * 	 * @param idCardNumber fullname accountType accountNumber state 	 * @param phone address 	 * @return list of accounts 	 */

--- FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
+++ FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
@@ -9,6 +9,7 @@ import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Component;
 
 import com.csc.fresher.controller.EntityManagerFactoryUtil;
+import com.csc.fresher.domain.Account;
 import com.csc.fresher.domain.SystemAccount;
 
 /*@author TrinhLe
@@ -39,46 +40,6 @@ public class SystemAccountDAO {
 		return systemaccounts;
 	}
 
-	/**
-	 * Login systemAccount
-	 * 
-	 * @param username
-	 * @param password
-	 * @return true if username.getPassword == password
-	 */
-	public boolean getSystemAccount(String username, String password) {
-
-		// Obtains entity manager object
-		EntityManager entityManager = EntityManagerFactoryUtil
-				.createEntityManager();
-
-		EntityTransaction entityTransaction = entityManager.getTransaction();
-
-		// -----------Begin transaction-----------
-		boolean check = false;
-		try {
-			entityTransaction.begin();
-
-			TypedQuery<SystemAccount> query = entityManager
-					.createQuery(
-							"SELECT c FROM "
-									+ SystemAccount.class.getName()
-									+ " c Where c.username= :username and c.password= :password",
-							SystemAccount.class);
-			query.setParameter("username", username);
-			query.setParameter("password", password);
-			SystemAccount sys = query.getSingleResult();
-			System.out.println(sys.getUsername());
-			check = query.getResultList().size() > 0;
-			entityTransaction.commit();
-		} catch (Exception e) {
-			entityManager.close(); 
-		}
-		// -----------End transaction-----------
-
-		return check;
-	}
-
 	public boolean addSystemAccount(SystemAccount systemAccount) {
 		// Obtains entity manager object
 		EntityManager entityManager = EntityManagerFactoryUtil
@@ -95,5 +56,96 @@ public class SystemAccountDAO {
 		entityTransaction.commit();
 		
 		return result;
+	}
+	
+	/**@author TrinhLe
+	 * Login systemAccount
+	 * 
+	 * @param username
+	 * @param password
+	 * @return systemAccount
+	 */
+	public SystemAccount getSystemAccount(String username, String password) {
+
+		// Obtains entity manager object
+		EntityManager entityManager = EntityManagerFactoryUtil
+				.createEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		SystemAccount systemAccount = new SystemAccount();
+		// -----------Begin transaction-----------
+		// boolean check = false;
+		try {
+			entityTransaction.begin();
+
+			TypedQuery<SystemAccount> query = entityManager
+					.createQuery(
+							"SELECT c FROM "
+									+ SystemAccount.class.getName()
+									+ " c Where c.username= :username and c.password= :password",
+							SystemAccount.class);
+			query.setParameter("username", username);
+			query.setParameter("password", password);
+			systemAccount = query.getSingleResult();
+			System.out.println(systemAccount.getUsername());
+			// check = query.getResultList().size() > 0;
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityManager.close(); 
+		}
+		// -----------End transaction-----------
+
+		return systemAccount;
+
+	}
+
+	/**@author TrinhLe
+	 * search account for 7 fields condition
+	 * 
+	 * @param idCardNumber fullname accountType accountNumber state
+	 * @param phone address
+	 * @return list of accounts
+	 */
+	// ID Card number, Name, Account type/number, state, phone & address
+	public List<Account> getAccount(String idCardNumber, String fullname,
+			int accountType, String accountNumber, int state,
+			String phone, String address) {
+
+		// Obtains entity manager object
+		EntityManager entityManager = EntityManagerFactoryUtil
+				.createEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		List<Account> accounts = null;
+		// -----------Begin transaction-----------
+		// boolean check = false;
+		try {
+			entityTransaction.begin();
+
+			TypedQuery<Account> query = entityManager
+					.createQuery(
+							"SELECT c FROM "
+									+ Account.class.getName()
+									+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber AND c.accountstate = "+state+" AND c.accounttype = " + accountType,
+							Account.class);
+			query.setParameter("fullname", "%" + fullname + "%");
+			query.setParameter("idCardNumber", "%" + idCardNumber + "%");
+			query.setParameter("phone", "%" + phone + "%");
+			query.setParameter("address", "%" + address + "%");
+			query.setParameter("accountNumber", "%" + accountNumber + "%");
+//			query.setParameter("state", Integer.parseInt(state));
+//			query.setParameter("accountType", Integer.parseInt(accountType));
+			// query.setParameter("password", password);
+			accounts = (List<Account>) query.getResultList();
+			// System.out.println(systemAccount.getUsername());
+			// check = query.getResultList().size() > 0;
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityManager.close();
+		}
+		// -----------End transaction-----------
+
+		return accounts;
+
 	}
 }
