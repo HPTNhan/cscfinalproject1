16c1e2a673b37146668bcec3b8f51bc3738d6c1f
letrungthientrinh <letrungthientrinh@gmail.com>
2014-10-20 08:26:06 +0000

    merger search page and list accounts

--- FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
+++ FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
@@ -119,6 +119,23 @@ public class SystemAccountDAO {
 
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 		List<Account> accounts = null;
+		String sql = "SELECT c FROM "
+				+ Account.class.getName()
+				+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) "
+				+ "AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) "
+				+ "AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber ";
+		if(state > 0 && accountType > 0){
+			sql += "AND c.accountstate = "
+					+ state + " AND c.accounttype = "
+					+ accountType;
+		} else if(state == 0 && accountType > 0){
+			sql += " AND c.accounttype = "
+					+ accountType;
+		}else if(accountType == 0  && state > 0){
+			sql += "AND c.accountstate = "
+					+ state;
+		}
+			
 		// -----------Begin transaction-----------
 		// boolean check = false;
 		try {
@@ -126,14 +143,15 @@ public class SystemAccountDAO {
 
 			TypedQuery<Account> query = entityManager
 					.createQuery(
-							"SELECT c FROM "
-									+ Account.class.getName()
-									+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) "
-									+ "AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) "
-									+ "AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber "
-									+ "AND c.accountstate = "
-									+ state + " AND c.accounttype = "
-									+ accountType, Account.class);
+//							"SELECT c FROM "
+//									+ Account.class.getName()
+//									+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) "
+//									+ "AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) "
+//									+ "AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber "
+//									+ "AND c.accountstate = "
+//									+ state + " AND c.accounttype = "
+//									+ accountType
+									sql, Account.class);
 			query.setParameter("fullname", "%" + fullname + "%");
 			query.setParameter("idCardNumber", "%" + idCardNumber + "%");
 			query.setParameter("phone", "%" + phone + "%");
@@ -145,6 +163,46 @@ public class SystemAccountDAO {
 			accounts = (List<Account>) query.getResultList();
 			// System.out.println(systemAccount.getUsername());
 			// check = query.getResultList().size() > 0;
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityManager.close();
+		}
+		// -----------End transaction-----------
+
+		return accounts;
+
+	}
+	
+	/**
+	 * @author TrinhLe search account for 7 fields condition
+	 * 
+	 * @param idCardNumber
+	 *            fullname accountType accountNumber state
+	 * @param phone
+	 *            address
+	 * @return list of accounts
+	 */
+	// ID Card number, Name, Account type/number, state, phone & address
+	public List<Account> getAccountsBaseOnDate() {
+
+		// Obtains entity manager object
+		EntityManager entityManager = EntityManagerFactoryUtil
+				.createEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		List<Account> accounts = null;
+		String sql = "SELECT c FROM "
+				+ Account.class.getName()
+				+ " c ORDER BY (c.timeStamp) DESC";
+		
+		// -----------Begin transaction-----------
+		// boolean check = false;
+		try {
+			entityTransaction.begin();
+
+			TypedQuery<Account> query = entityManager
+					.createQuery(sql, Account.class);
+			accounts = (List<Account>) query.getResultList();
 			entityTransaction.commit();
 		} catch (Exception e) {
 			entityManager.close();
