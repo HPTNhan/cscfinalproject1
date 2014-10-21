da29b8beda89d6d71bb40ebd0ceeb965d6a3005e
letrungthientrinh@gmail.com <letrungthientrinh@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-21 09:28:05 +0000

    controll for multi-select

--- FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
+++ FinalProject1/src/main/java/com/csc/fresher/dao/SystemAccountDAO.java
@@ -99,7 +99,7 @@ public class SystemAccountDAO {
 
 	}
 
-	/**
+	/**not use multi-select for accountType and accountState
 	 * @author TrinhLe search account for 7 fields condition
 	 * 
 	 * @param idCardNumber
@@ -108,8 +108,7 @@ public class SystemAccountDAO {
 	 *            address
 	 * @return list of accounts
 	 */
-	// ID Card number, Name, Account type/number, state, phone & address
-	public List<Account> getAccount(String idCardNumber, String fullname,
+	/*public List<Account> getAccount(String idCardNumber, String fullname,
 			int accountType, String accountNumber, int state, String phone,
 			String address) {
 
@@ -142,27 +141,100 @@ public class SystemAccountDAO {
 			entityTransaction.begin();
 
 			TypedQuery<Account> query = entityManager
-					.createQuery(
-//							"SELECT c FROM "
-//									+ Account.class.getName()
-//									+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) "
-//									+ "AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) "
-//									+ "AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber "
-//									+ "AND c.accountstate = "
-//									+ state + " AND c.accounttype = "
-//									+ accountType
-									sql, Account.class);
+					.createQuery(sql, Account.class);
 			query.setParameter("fullname", "%" + fullname + "%");
 			query.setParameter("idCardNumber", "%" + idCardNumber + "%");
 			query.setParameter("phone", "%" + phone + "%");
 			query.setParameter("address", "%" + address + "%");
 			query.setParameter("accountNumber", "%" + accountNumber + "%");
-			// query.setParameter("state", Integer.parseInt(state));
-			// query.setParameter("accountType", Integer.parseInt(accountType));
-			// query.setParameter("password", password);
 			accounts = (List<Account>) query.getResultList();
-			// System.out.println(systemAccount.getUsername());
-			// check = query.getResultList().size() > 0;
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityManager.close();
+		}
+		// -----------End transaction-----------
+
+		return accounts;
+
+	}*/
+	/**
+	 * @author TrinhLe search account for 7 fields condition
+	 * 
+	 * @param idCardNumber
+	 *            fullname accountType accountNumber state
+	 * @param phone
+	 *            address
+	 * @return list of accounts
+	 */
+	public List<Account> getAccount(String idCardNumber, String fullname,
+			String[] accountType, String accountNumber, String[] state, String phone,
+			String address) {
+
+		// Obtains entity manager object
+		EntityManager entityManager = EntityManagerFactoryUtil
+				.createEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		List<Account> accounts = null;
+		int i;
+		String sql = "SELECT c FROM "
+				+ Account.class.getName()
+				+ " c WHERE (c.firstName LIKE :fullname OR c.lastName LIKE :fullname OR c.midName LIKE :fullname) "
+				+ "AND c.idCardNumber LIKE :idCardNumber AND (c.phoneNumber1 LIKE :phone OR c.phoneNumber2 LIKE :phone) "
+				+ "AND (c.address1 LIKE :address OR c.address2 LIKE :address) AND c.accountNumber LIKE :accountNumber AND c.isDeleted = 'false' ";
+		if(state.length > 0 && accountType.length > 0){
+			sql += "AND ( ";
+			for(i=0; i<state.length;i++){
+				sql += "c.accountstate = "
+					+ Integer.parseInt(state[i]);
+				if(i<state.length-1){
+					sql += " OR ";
+				}
+			}
+			sql += ") AND ( ";
+			for(i=0; i<accountType.length;i++){
+				sql += "c.accounttype = "
+					+ Integer.parseInt(accountType[i]);
+				if(i<accountType.length-1){
+					sql += " OR ";
+				}
+			}
+			sql += " )";
+		} else if(state.length == 0 && accountType.length > 0){
+			sql += "AND ( ";
+			for(i=0; i<accountType.length;i++){
+				sql += "c.accounttype = "
+					+ Integer.parseInt(accountType[i]);
+				if(i<accountType.length-1){
+					sql += " OR ";
+				}
+			}
+			sql += " )";
+		}else if(accountType.length == 0  && state.length > 0){
+			sql += "AND ( ";
+			for(i=0; i<state.length;i++){
+				sql += "c.accountstate = "
+					+ Integer.parseInt(state[i]);
+				if(i<state.length-1){
+					sql += " OR ";
+				}
+			}
+			sql += " )";
+		}
+			
+		// -----------Begin transaction-----------
+		// boolean check = false;
+		try {
+			entityTransaction.begin();
+
+			TypedQuery<Account> query = entityManager
+					.createQuery(sql, Account.class);
+			query.setParameter("fullname", "%" + fullname + "%");
+			query.setParameter("idCardNumber", "%" + idCardNumber + "%");
+			query.setParameter("phone", "%" + phone + "%");
+			query.setParameter("address", "%" + address + "%");
+			query.setParameter("accountNumber", "%" + accountNumber + "%");
+			accounts = (List<Account>) query.getResultList();
 			entityTransaction.commit();
 		} catch (Exception e) {
 			entityManager.close();
