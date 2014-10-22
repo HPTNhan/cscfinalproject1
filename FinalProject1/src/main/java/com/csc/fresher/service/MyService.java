95c163f7214385f08ccf5fbebdb7aaa597ca0b2f
letrungthientrinh@gmail.com <letrungthientrinh@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-22 07:09:29 +0000

    add methods: checkLogin, searchAccountsBaseOnDate, searchAccounts

--- FinalProject1/src/main/java/com/csc/fresher/service/MyService.java
+++ FinalProject1/src/main/java/com/csc/fresher/service/MyService.java
@@ -1,25 +1,85 @@
 package com.csc.fresher.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.csc.fresher.dao.AccountDAO;
+import com.csc.fresher.dao.SystemAccountDAO;
+import com.csc.fresher.domain.Account;
 
 @Service
 public class MyService {
-	
-	//@Autowired
-	private AccountDAO accountDAO = new AccountDAO();
-	
-	private MyService(){
-		
+
+	@Autowired
+	private AccountDAO accountDAO;
+	@Autowired
+	private SystemAccountDAO systemAccountDAO;
+
+	private MyService() {
+
 	}
-	
+
 	public boolean deleteListAccount(String[] listIdAccount) {
 		if (listIdAccount.length > 0) {
 			return accountDAO.deleteListAccount(listIdAccount);
 		} else {
 			return false;
-		}		
+		}
 	}
-	
+
+	public String checkLogin(String username, String password) {
+		if (systemAccountDAO.getSystemAccount(username, password).getId() > 0) {
+			return systemAccountDAO.getSystemAccount(username, password)
+					.getRole();
+		} else {
+			return "false";
+		}
+	}
+
+	public List<Account> searchAccountsBaseOnDate() {
+		List<Account> accounts = new ArrayList<Account>();
+		if (systemAccountDAO.getAccountsBaseOnDate().size() > 0) {
+			List<Account> listAcc = systemAccountDAO.getAccountsBaseOnDate();
+			for (int i = 0; i < 10; i++) {
+				Account account = listAcc.get(i);
+				// temp += listAcc.get(i).getAccountstate().getIdstate();
+				accounts.add(account);
+			}
+			return accounts;
+		} else {
+			return null;
+		}
+	}
+
+	public List<Account> searchAccounts(String idCardNumber, String fullname,
+			String[] accountType, String accountNumber, String[] state,
+			String phone, String address) {
+		if (systemAccountDAO.getAccounts(idCardNumber, fullname, accountType,
+				accountNumber, state, phone, address).size() > 0) {
+			return systemAccountDAO.getAccounts(idCardNumber, fullname,
+					accountType, accountNumber, state, phone, address);
+		} else {
+			return null;
+		}
+	}
+
+	public String getStateForAccountList(String[] state) {
+		String flat = "";
+		String temp = "";
+		if (state != null) {
+			for (int i = 0; i < state.length; i++) {
+				temp += state[i];
+			}
+			if (temp.contains("2") && temp.contains("1") && temp.contains("3"))
+				flat = "true";
+			else if (temp.contains("1") && temp.contains("3"))
+				flat = "false";
+		} else
+			flat = "true";
+		return flat;
+	}
+
 }
