380efa8abc6b1bb69055c557714086bd2067ae7a
letrungthientrinh@gmail.com <letrungthientrinh@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-27 07:07:50 +0000

    add AOP to get notification

---
+++ FinalProject1/src/main/java/com/csc/fresher/aspect/NotifyAOP.java
@@ -0,0 +1,44 @@
+package com.csc.fresher.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+import com.csc.fresher.service.MyService;
+/**
+ * @author TrinhLe
+ * 
+ *
+ */
+@Component
+@Aspect
+public class NotifyAOP {
+
+	@Autowired
+	private MyService service;
+	
+		
+	@Before("execution(* com.csc.fresher.controller.*AccountController.*(..))  && args(*, model)")
+	public void getMethodName(JoinPoint joinPoint, Model model){
+		
+		System.out.println("Method name: " + joinPoint.getSignature().getName());
+		model.addAttribute("newAccount", service
+				.getSizeAccountsBaseOnState(service
+						.searchAccountsBaseOnState(1)));
+		model.addAttribute("activeAccount", service
+				.getSizeAccountsBaseOnState(service
+						.searchAccountsBaseOnState(2)));
+		model.addAttribute("disableAccount", service
+				.getSizeAccountsBaseOnState(service
+						.searchAccountsBaseOnState(3)));
+		model.addAttribute("removableAccount", service
+				.getSizeAccountsBaseOnState(service
+						.searchAccountsBaseOnState(4)));
+		System.out.println("===========================================");
+	}
+
+
+}
