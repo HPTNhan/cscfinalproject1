75f7e8c7fafb819be6f038acf5aab8d1cc6ad40f
hpthainhan@gmail.com <hpthainhan@gmail.com@de341f6e-cadb-1ad7-d7dc-41c4bf7ac948>
2014-10-30 03:14:59 +0000

    fix warning

--- FinalProject1/src/main/java/com/csc/fresher/domain/AccountType.java
+++ FinalProject1/src/main/java/com/csc/fresher/domain/AccountType.java
@@ -1,11 +1,16 @@
 package com.csc.fresher.domain;
 
 import java.io.Serializable;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 /**
